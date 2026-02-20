ce1d1938efcf2f437d8db4b288c6a1800eceeb5d
chamithsandeepa <chamithsandeepa321@gmail.com>
2026-02-20 12:43:05 +0530

    Add ScrollToTop component for improved navigation experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home, AboutPage, ServicesPage, ProcessPage, ProjectsPage, ContactPage } from './pages';
+import { ScrollToTop } from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutPage />} />
